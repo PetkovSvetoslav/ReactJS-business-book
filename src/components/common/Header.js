6607a30ba7d5fee95a5001924772b18fdfe104d5
Svetoslav Krasimirov Petkov <98747798+PetkovSvetoslav@users.noreply.github.com>
2022-08-05 14:59:12 +0300

    Add files via upload

---
+++ src/components/common/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Header = ({handleToggleDarkMode}) => {
+    return (
+        <div className="header">
+            
+             {/*the heading of our notes part*/}
+
+        <h1><span style={{color:"#00d0ff"}}>My Business</span> Notes</h1>
+
+        </div>
+    )
+}
+
+export default Header
