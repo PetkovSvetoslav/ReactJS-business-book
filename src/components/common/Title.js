8e5031c0247d8693c72f6e1c4d139b1e7f96f6cd
Svetoslav Krasimirov Petkov <98747798+PetkovSvetoslav@users.noreply.github.com>
2022-08-05 14:59:28 +0300

    Add files via upload

---
+++ src/components/common/Title.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Title = ({handleToggleDarkMode}) => {
+    return (
+        <div className="headerT">
+            
+             {/*the title of our create partners part*/}
+
+        <h1><span style={{color:"#00d0ff"}}>My Business </span> Partners</h1>
+
+        </div>
+    );
+}
+
+export default Title
